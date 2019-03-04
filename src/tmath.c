ba64025cac2ee78168cf9b605600628f0adee831
Surafel Assefa <surafel911@gmail.com>
2019-03-03 23:04:48 -0500

    Fixes out of bounds for loop.

--- src/tmath.c
+++ src/tmath.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define ARRAY_SIZE(x) (sizeof(x)/sizeof(*x))
+
 static const char* _delimiters[] = {
 	"^",
 	"*/",
@@ -19,24 +21,22 @@ enum tmath_op {
 };
 
 struct tmath_ast_node {
+	const char* lhs, rhs;
 	struct tmath_ast_node* ast;
-	double lhs, rhs;
 	enum tmath_op op;
 };
 
 static bool
 tmath_has_op(const char* exp)
 {
-	const char* tmp;
-	while (*tmp > '\0') {
-		switch (*tmp) {
+	for (const char* c = exp; c != '\0'; c++) {
+		switch (*c) {
 		case TMATH_OP_EXP:
 		case TMATH_OP_MUL:
 		case TMATH_OP_DIV:
 		case TMATH_OP_ADD:
 		case TMATH_OP_SUB:
 			return true;
-			break;
 		}
 	}
 
@@ -49,14 +49,14 @@ tmath_parse(const char* exp)
 	char* tok;
 	const char* op;
 	struct tmath_ast_node* ast = NULL;
-	size_t len;
 
 	// TODO: strtok likely replaces delimiters with '\0'. Find a way around that
 
-	len = strlen(exp);
-	for (int i = 0; i < sizeof(_delimiters); i++) {
+	for (int i = 0; i < ARRAY_SIZE(_delimiters); i++) {
 		op = _delimiters[i];
 		tok = strtok((char*)exp, op);
+
+		tmath_has_op("");
 
 		while (tok != NULL) {
 			printf("%s: %s\n", op, tok);
